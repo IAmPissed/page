3f6656997c31b8bf0cdab640f53c50ef404b354b
xaniar <joker427861@gmail.com>
2021-11-12 15:08:03 +0330

    add modules, flags, and output password

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
+const { program } = require('commander');
+const chalk = require('chalk');
+const clipboard = require('clipboardy');
+const passgen = require('./passgen');
+const log = console.log;
 
-console.log('Hello form Node JS!!!');
+// Setting version and description
+program
+    .version('1.0.0')
+    .description('🧰 Password Generator 🧰');
+
+// Adding options(flags)
+program
+    .option('--length <number>', 'specify the length', '8')
+    .option('-ns,--no-special', 'no special characters included')
+    .option('-nn,--no-number', 'no numbers included')
+    .option('-s, --save', 'save to passwords.txt', false)
+    .parse(process.argv);
+
+const options = program.opts();
+
+// Generating the password using flags 
+let password = passgen(options.length, options.number, options.special);
+
+// Saving to clipboard
+clipboard.writeSync(password)
+
+// Output password
+log(chalk.cyanBright('Generated Password --> ') + chalk.rgb(255, 79, 167)(password));
+// confirmation message
+log(chalk.rgb(255, 153, 145)('Password copied to clipboard!'));
